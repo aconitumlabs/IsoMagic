636c1ae5f3041fad3c18303dbef0cecd0f747534
ludihan <65617704+ludihan@users.noreply.github.com>
2025-10-10 12:23:33 -0300

    feat: generate whole ramp

--- Script/ramp.gd
+++ Script/ramp.gd
@@ -1,27 +1,59 @@
 @tool
 extends Node3D
 
+const initial_mesh :PackedVector3Array = [
+	# front slope
+	Vector3(0.5,-0.5,0.5),
+	Vector3(-0.5,0.5,-0.5),
+	Vector3(0.5,-0.5,-0.5),
+
+	Vector3(-0.5,0.5,-0.5),
+	Vector3(0.5,-0.5,0.5),
+	Vector3(-0.5,0.5,0.5),
+
+	# right side
+	Vector3(0.5,-0.5,-0.5),
+	Vector3(-0.5,0.5,-0.5),
+	Vector3(-0.5,-0.5,-0.5),
+
+	# back side
+	Vector3(-0.5,-0.5,-0.5),
+	Vector3(-0.5,0.5,0.5),
+	Vector3(-0.5,-0.5,0.5),
+	
+	Vector3(-0.5,-0.5,-0.5),
+	Vector3(-0.5,0.5,-0.5),
+	Vector3(-0.5,0.5,0.5),
+
+	# left side
+	Vector3(-0.5,-0.5,0.5),
+	Vector3(-0.5,0.5,0.5),
+	Vector3(0.5,-0.5,0.5),
+
+	# bottom
+	Vector3(0.5,-0.5,0.5),
+	Vector3(0.5,-0.5,-0.5),
+	Vector3(-0.5,-0.5,-0.5),
+
+	Vector3(0.5,-0.5,0.5),
+	Vector3(-0.5,-0.5,-0.5),
+	Vector3(-0.5,-0.5,0.5),
+]
+
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 
 @export_range(0, 7) var direction: int = 0:
 	get:
 		return direction
 	set(value):
+		if mesh_instance_3d == null: 
+			return
 		direction = value
-		var triangles := PackedVector3Array([
-			Vector3(0.5,-0.5,0.5),
-			Vector3(-0.5,0.5,-0.5),
-			Vector3(0.5,-0.5,-0.5),
-			Vector3(-0.5,0.5,-0.5),
-			Vector3(0.5,-0.5,0.5),
-			Vector3(-0.5,0.5,0.5),
-		])
-		_gen_mesh(triangles)
-		
+		_gen_mesh(initial_mesh)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	_gen_mesh(initial_mesh)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -30,7 +62,12 @@ func _process(delta: float) -> void:
 	
 func _gen_mesh(triangles: PackedVector3Array) -> void:
 	var st := SurfaceTool.new()
+	
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	st.set_color(Color.BLUE)
 	for t in triangles:
+		st.set_smooth_group(-1)
 		st.add_vertex(t)
+	st.generate_normals()
+	st.index()
 	mesh_instance_3d.mesh = st.commit()
