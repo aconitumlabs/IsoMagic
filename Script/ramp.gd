e29a2dee45ea4626c05ddcb2a5120e8e3279841b
ludihan <65617704+ludihan@users.noreply.github.com>
2025-10-14 16:34:03 -0300

    feat: color ramp based on normal vectors

--- Script/ramp.gd
+++ Script/ramp.gd
@@ -1,7 +1,17 @@
 @tool
-extends Node3D
+extends MeshInstance3D
 
-const initial_mesh :PackedVector3Array = [
+@export_tool_button("Build mesh", "BoxMesh") var build_mesh = gen_mesh
+
+@export_group("Colors")
+@export var positive_x_color := Color(1, 0.25, 0.25) #FF4040
+@export var negative_x_color := Color(1.0, 0.23,1.0 ) #FF3AFF
+@export var positive_y_color := Color(0.24, 0.25, 1.0) #3C40FF
+@export var negative_y_color := Color(0.21, 1.0, 1.0 ) #36FFFF
+@export var positive_z_color := Color(1.0, 1.0, 0.26) #FFFF42
+@export var negative_z_color := Color(1.0, 0.78, 0.26) #FFC642
+
+const triangles :PackedVector3Array = [
 	# front slope
 	Vector3(0.5,-0.5,0.5),
 	Vector3(-0.5,0.5,-0.5),
@@ -40,34 +50,56 @@ const initial_mesh :PackedVector3Array = [
 	Vector3(-0.5,-0.5,0.5),
 ]
 
-@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
-
-@export_range(0, 7) var direction: int = 0:
-	get:
-		return direction
-	set(value):
-		if mesh_instance_3d == null: 
-			return
-		direction = value
-		_gen_mesh(initial_mesh)
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_gen_mesh(initial_mesh)
+	gen_mesh()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	#rotation += Vector3(0,PI * delta,0)
+	#rotation += Vector3(-PI * delta,PI * delta,PI * delta)
+	#print(Quaternion(basis))
 	pass
-	
-func _gen_mesh(triangles: PackedVector3Array) -> void:
+
+func gen_mesh() -> void:
+	assert(
+		len(triangles) % 3 == 0,
+		"ERROR: Mesh needs to specified in an array of triangles (3 vertices for each face)"
+	)
+
 	var st := SurfaceTool.new()
-	
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-	st.set_color(Color.BLUE)
+
+
+	var plane :PackedVector3Array = []
+	var count := 0
+
 	for t in triangles:
-		st.set_smooth_group(-1)
-		st.add_vertex(t)
-	st.generate_normals()
+		plane.append(t)
+		count += 1
+		if count == 3:
+			# uso get e nao [] para nao crashar o @tool caso eu faça merda
+			var a := plane.get(0)
+			var b := plane.get(1)
+			var c := plane.get(2)
+			var normal := Plane(a,b,c).normal
+
+			for v in [a,b,c]:
+				st.set_normal(normal)
+				var color := Color()
+				var transformed_normal := basis * normal
+				var x := transformed_normal.x**2
+				var y := transformed_normal.y**2
+				var z := transformed_normal.z**2
+				color += x * positive_x_color if transformed_normal.x >= 0 \
+					else x * negative_x_color
+				color += y * positive_y_color if transformed_normal.y >= 0 \
+					else y * negative_y_color
+				color += z * positive_z_color if transformed_normal.z >= 0 \
+					else z * negative_z_color
+				st.set_smooth_group(-1)
+				st.set_color(color)
+				st.add_vertex(v)
+			count = 0
+			plane.clear()
 	st.index()
-	mesh_instance_3d.mesh = st.commit()
+	mesh = st.commit()
