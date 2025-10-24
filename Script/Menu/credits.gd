12e59c6145d8d4e2f6add11356a429b0a5f321db
ludihan <65617704+ludihan@users.noreply.github.com>
2025-10-24 03:22:11 -0300

    refactor: user animation player for fade in and fade out of credits screen

--- Script/Menu/credits.gd
+++ Script/Menu/credits.gd
@@ -1,22 +1,26 @@
 extends ColorRect
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func play_intro_animation() -> void:
-	if not material:
-		push_error("Este ColorRect (Credits) não tem um ShaderMaterial aplicado!")
-		return
-
-	(material as ShaderMaterial).set_shader_parameter("progress", 0.0)
-	
-	show()
-
-	var tween := create_tween()
-	tween.tween_property(material, "shader_parameter/progress", 1.1, 0.6)
+	animation_player.play("appear")
+	#if not material:
+		#push_error("Este ColorRect (Credits) não tem um ShaderMaterial aplicado!")
+		#return
+#
+	#(material as ShaderMaterial).set_shader_parameter("progress", 0.0)
+	#
+	#show()
+#
+	#var tween := create_tween()
+	#tween.tween_property(material, "shader_parameter/progress", 1.1, 0.6)
 
 func _on_back_button_pressed() -> void:
-	if not material:
-		return
-
-	var tween := create_tween()
-	tween.tween_property(material, "shader_parameter/progress", 0.0, 0.6)
+	animation_player.play("disappear")
+	#if not material:
+		#return
+#
+	#var tween := create_tween()
+	#tween.tween_property(material, "shader_parameter/progress", 0.0, 0.6)
 	
-	tween.finished.connect(hide)
+	#tween.finished.connect(hide)
